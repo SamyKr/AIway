1aca70d8314f56b4ded0e94f7d4a1b3266c2c56b
SamyKr <samyaymane@gmail.com>
2025-04-23 15:58:24 -0400

    Initial Commit

---
+++ AIway/AIwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIwayApp.swift
+//  AIway
+//
+//  Created by Samy 📍 on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIwayTests/AIwayTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIwayTests.swift
+//  AIwayTests
+//
+//  Created by Samy 📍 on 23/04/2025.
+//
+
+import Testing
+@testable import AIway
+
+struct AIwayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
